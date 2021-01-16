f3e5b7b26b947a0fd191fe85732fcc2e639ca297
Lachlan McKee <lachlandevelopment@gmail.com>
2021-01-16 12:10:23 +0000

    Issue #64 Added home test for second configuration

--- backend/src/test/kotlin/net/lachlanmckee/flankci/presentation/ApplicationTest.kt
+++ backend/src/test/kotlin/net/lachlanmckee/flankci/presentation/ApplicationTest.kt
@@ -10,20 +10,35 @@ import kotlin.test.assertEquals
 internal class ApplicationTest {
 
   @Test
-  fun testHomeRequest() = withTestApplication(createTestHttpClientFactory()) {
+  fun testHomeRequestWithSingleConfiguration() = withTestApplication(
+    createTestHttpClientFactory(),
+    configFileName = "config.json"
+  ) {
     with(handleRequest(HttpMethod.Get, "/")) {
       assertEquals(HttpStatusCode.OK, response.status())
-      assertContentEquals(response, "output/home/expected.html")
+      assertContentEquals(response, "output/home/expected-with-single-config.html")
+    }
+  }
+
+  @Test
+  fun testHomeRequestWithMultipleConfigurations() = withTestApplication(
+    createTestHttpClientFactory(),
+    configFileName = "config-with-multiple-configurations.json"
+  ) {
+    with(handleRequest(HttpMethod.Get, "/")) {
+      assertEquals(HttpStatusCode.OK, response.status())
+      assertContentEquals(response, "output/home/expected-with-multiple-configs.html")
     }
   }
 
   private fun <R> withTestApplication(
     testHttpClientFactory: TestHttpClientFactory,
+    configFileName: String,
     test: TestApplicationEngine.() -> R
   ): R {
     val component: ApplicationComponent = DaggerApplicationComponent
       .builder()
-      .coreIoModule(CoreIoModule(testHttpClientFactory, TestFileReader()))
+      .coreIoModule(CoreIoModule(testHttpClientFactory, TestFileReader(mapOf("config.json" to configFileName))))
       .build()
 
     return withTestApplication(component.typeAdapterFactories(), component.routeProviders(), test)
